3e4ea5806561d9434a351740d0a0c8bea11a4d70
Michael Jaser <michael.jaser@peerigon.com>
2019-08-19 17:34:46 +0200

    Updates

---
+++ slides/precache-manifest.cbd381b71b23c2e75b62936b2d61e4a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b3192d12f5c15b146c2",
+    "url": "/munich/slides/static/js/main.9b3192d1.chunk.js"
+  },
+  {
+    "revision": "a170133eafc767a4b5b8",
+    "url": "/munich/slides/static/css/1.3a17b599.chunk.css"
+  },
+  {
+    "revision": "a170133eafc767a4b5b8",
+    "url": "/munich/slides/static/js/1.a170133e.chunk.js"
+  },
+  {
+    "revision": "f83642fb948d45f6e20e",
+    "url": "/munich/slides/static/js/runtime~main.f83642fb.js"
+  },
+  {
+    "revision": "1a4ce3dad151b6f3fbbc3121a5b6822f",
+    "url": "/munich/slides/index.html"
+  }
+];
